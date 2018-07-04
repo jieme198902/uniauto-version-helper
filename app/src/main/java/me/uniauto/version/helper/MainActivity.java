8ccfa70bd762f863bd74b57737a0087b970a4a3f
jieme198902 <jieme1989@163.com>
2018-07-04 14:46:08 +0800

    去掉不用的依赖。

--- app/src/main/java/me/uniauto/version/helper/MainActivity.java
+++ app/src/main/java/me/uniauto/version/helper/MainActivity.java
@@ -1,13 +1,13 @@
 package me.uniauto.version.helper;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import me.uniauto.version.entity.CUpdateAppBean;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
